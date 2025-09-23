5190a507d9e79ddbebacc6ec87c0f6a9d3202645
bathtime <andresmail@protonmail.com>
2025-09-23 13:38:10 -0400

    Updating

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 /*
-
  git add .; git commit -m 'Updating'; git checkout main; git push
  macroquad = "0.4"
  use macroquad::{miniquad::ElapsedQuery, prelude::*};
  use macroquad::{experimental, miniquad::*, prelude::*};
-
 */
 
 use macroquad::{miniquad::*, prelude::*};
 
 
 
+
 const GAME_SPEED: f32 = 1.0;
-const PLAYER_WIDTH: f32 = 27.0;
+const PLAYER_WIDTH: f32 = 37.0;
 const PLAYER_HEIGHT: f32 = 50.0;
 const BASE_MOVE_SPEED: f32 = 200.0;
 const RUNNING_SPEED: f32 = 300.0;
